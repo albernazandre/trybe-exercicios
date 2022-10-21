455d02bd619f2ac40c15512a1c5ab9aacd258224
andre <andrespeltzalbernaz@gmail.com>
2022-10-21 16:22:20 -0300

    ultima alteracoes no 4.1

--- fundamentos/secao4/dia-1-intro-ao-js/exercicios.js
+++ fundamentos/secao4/dia-1-intro-ao-js/exercicios.js
@@ -146,3 +146,15 @@ else if (salarioLiqInss>faixaTetoSalarioIr) {
 else {
     console.log("Valor de salário líquido de IR inválido!");
 }
+
+
+
+
+function dice () { 
+    return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+    d2 = dice();
+}
