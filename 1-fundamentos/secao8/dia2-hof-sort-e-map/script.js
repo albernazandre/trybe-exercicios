3035133e2aab5e96491ed98e998246eb108d0808
andre <andrespeltzalbernaz@gmail.com>
2022-12-03 18:52:14 -0300

    ex pronto

---
+++ 1-fundamentos/secao8/dia2-hof-sort-e-map/script.js
@@ -0,0 +1,103 @@
+// exercicio 1
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+  const formatedBookNames = () => {
+    return books.map((book) => (`${book.name} - ${book.genre} - ${book.author.name}`));
+  }
+
+  const formatedAuthor = () => {
+    return books.map((book) => (`${book.author.name} - ${book.author.birthYear}`));
+  }
+
+  // Construa um array de objetos a partir do array de livros.
+  // Cada objeto deve conter uma propriedade author, com o nome da pessoa autora 
+  // do livro, e uma propriedade age, com a idade dessa pessoa quando o livro foi lançado. 
+  // O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha.
+
+  /*const nameAndAge = () => {
+  return books
+    .map((book) => (
+      {
+        author: book.author.name,
+        age: book.releaseYear - book.author.birthYear,
+      }
+    ))
+    .sort((obj1, obj2) => obj1.age - obj2.age);
+} */
+
+  const launchBook = books.map((author) => {
+    const launch = {};
+    launch.author = author.author.name;
+    launch.age = author.releaseYear - author.author.birthYear;
+    return launch;
+  })
+
+  const sortLaunch = launchBook.sort((a, b) => a.age - b.age);
+
+console.log(launchBook);
+console.log(sortLaunch);
+
